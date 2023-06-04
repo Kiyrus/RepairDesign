06fb626e655e9f1f2f2213af0e75e24aed7fe91d
Ilya Kosov <kosov.ilya@bk.ru>
2023-06-04 15:53:46 +0300

    code has been executed for the workability of the desktop version

--- js/carousel.js
+++ js/carousel.js
@@ -4,6 +4,9 @@ const buttons = document.querySelectorAll('.second-section__button');
 const handLeft = document.querySelector('.second-section__hand-left');
 const handRight = document.querySelector('.second-section__hand-right');
 const listButtons = document.querySelectorAll('.second-section__list-button');
+const replacementCitys = document.querySelectorAll('.replacementCity');
+const replacementApartmentAreas = document.querySelectorAll('.replacementApartmentArea');
+const replacementRepairTimes = document.querySelectorAll('.replacementRepairTime');
 
 // Функция для переключения слайдов
 function switchSlide(index) {
@@ -11,11 +14,17 @@ function switchSlide(index) {
     images.forEach(image => image.classList.remove('second-section__img--active'));
     buttons.forEach(button => button.classList.remove('second-section__button--active'));
     listButtons.forEach(listButton => listButton.classList.remove('second-section__list-button--active'));
+    replacementCitys.forEach(replacementCity => replacementCity.classList.remove('replacementCity--active'));
+    replacementApartmentAreas.forEach(replacementApartmentArea => replacementApartmentArea.classList.remove('replacementApartmentArea--active'));
+    replacementRepairTimes.forEach(replacementRepairTime => replacementRepairTime.classList.remove('replacementRepairTime--active'));
 
   // Добавляем класс "active" к выбранному слайду и кнопке
     images[index].classList.add('second-section__img--active');
     buttons[index].classList.add('second-section__button--active');
     listButtons[index].classList.add('second-section__list-button--active');
+    replacementCitys[index].classList.add('replacementCity--active');
+    replacementApartmentAreas[index].classList.add('replacementApartmentArea--active');
+    replacementRepairTimes[index].classList.add('replacementRepairTime--active');
 }
 
 // Обработчики событий для кнопок переключения слайдов
@@ -27,6 +36,24 @@ buttons.forEach((button, index) => {
 
 listButtons.forEach((listButton, index) => {
     listButton.addEventListener('click', () => {
+    switchSlide(index);
+    });
+});
+
+replacementCitys.forEach((replacementCity, index) => {
+    replacementCity.addEventListener('click', () => {
+    switchSlide(index);
+    });
+});
+
+replacementApartmentAreas.forEach((replacementApartmentArea, index) => {
+    replacementApartmentArea.addEventListener('click', () => {
+    switchSlide(index);
+    });
+});
+
+replacementRepairTimes.forEach((replacementRepairTime, index) => {
+    replacementRepairTime.addEventListener('click', () => {
     switchSlide(index);
     });
 });
@@ -48,3 +75,6 @@ handRight.addEventListener('click', () => {
 
 // По умолчанию показываем первый слайд
 switchSlide(0);
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
