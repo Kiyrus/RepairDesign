6e0972c021c8c6fa2dab421406ca174a0f940e24
Ilya Kosov <kosov.ilya@bk.ru>
2023-06-05 22:04:25 +0300

    formatting settings changed

--- js/carousel.js
+++ js/carousel.js
@@ -1,119 +1,85 @@
 const mobileImages = document.querySelectorAll(".second-section__img-mobile");
-const mobileHandLeft = document.querySelector(
-  ".second-section__mobile-slider-button-left"
-);
-const mobileHandRight = document.querySelector(
-  ".second-section__mobile-slider-button-right"
-);
+const mobileHandLeft = document.querySelector(".second-section__mobile-slider-button-left");
+const mobileHandRight = document.querySelector(".second-section__mobile-slider-button-right");
 const images = document.querySelectorAll(".second-section__img");
 const buttons = document.querySelectorAll(".second-section__button");
 const handLeft = document.querySelector(".second-section__hand-left");
 const handRight = document.querySelector(".second-section__hand-right");
 const listButtons = document.querySelectorAll(".second-section__list-button");
 const replacementCitys = document.querySelectorAll(".replacementCity");
-const replacementApartmentAreas = document.querySelectorAll(
-  ".replacementApartmentArea"
-);
-const replacementRepairTimes = document.querySelectorAll(
-  ".replacementRepairTime"
-);
+const replacementApartmentAreas = document.querySelectorAll(".replacementApartmentArea");
+const replacementRepairTimes = document.querySelectorAll(".replacementRepairTime");
 
 function switchSlide(index) {
-  mobileImages.forEach((mobileImage) =>
-    mobileImage.classList.remove("second-section__img-mobile--active")
-  );
-  images.forEach((image) =>
-    image.classList.remove("second-section__img--active")
-  );
-  buttons.forEach((button) =>
-    button.classList.remove("second-section__button--active")
-  );
-  listButtons.forEach((listButton) =>
-    listButton.classList.remove("second-section__list-button--active")
-  );
-  replacementCitys.forEach((replacementCity) =>
-    replacementCity.classList.remove("replacementCity--active")
-  );
-  replacementApartmentAreas.forEach((replacementApartmentArea) =>
-    replacementApartmentArea.classList.remove(
-      "replacementApartmentArea--active"
-    )
-  );
-  replacementRepairTimes.forEach((replacementRepairTime) =>
-    replacementRepairTime.classList.remove("replacementRepairTime--active")
-  );
+    mobileImages.forEach((mobileImage) => mobileImage.classList.remove("second-section__img-mobile--active"));
+    images.forEach((image) => image.classList.remove("second-section__img--active"));
+    buttons.forEach((button) => button.classList.remove("second-section__button--active"));
+    listButtons.forEach((listButton) => listButton.classList.remove("second-section__list-button--active"));
+    replacementCitys.forEach((replacementCity) => replacementCity.classList.remove("replacementCity--active"));
+    replacementApartmentAreas.forEach((replacementApartmentArea) => replacementApartmentArea.classList.remove("replacementApartmentArea--active"));
+    replacementRepairTimes.forEach((replacementRepairTime) => replacementRepairTime.classList.remove("replacementRepairTime--active"));
 
-  mobileImages[index].classList.add("second-section__img-mobile--active");
-  images[index].classList.add("second-section__img--active");
-  buttons[index].classList.add("second-section__button--active");
-  listButtons[index].classList.add("second-section__list-button--active");
-  replacementCitys[index].classList.add("replacementCity--active");
-  replacementApartmentAreas[index].classList.add(
-    "replacementApartmentArea--active"
-  );
-  replacementRepairTimes[index].classList.add("replacementRepairTime--active");
+    mobileImages[index].classList.add("second-section__img-mobile--active");
+    images[index].classList.add("second-section__img--active");
+    buttons[index].classList.add("second-section__button--active");
+    listButtons[index].classList.add("second-section__list-button--active");
+    replacementCitys[index].classList.add("replacementCity--active");
+    replacementApartmentAreas[index].classList.add("replacementApartmentArea--active");
+    replacementRepairTimes[index].classList.add("replacementRepairTime--active");
 }
 
 buttons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    switchSlide(index);
-  });
+    button.addEventListener("click", () => {
+        switchSlide(index);
+    });
 });
 
 listButtons.forEach((listButton, index) => {
-  listButton.addEventListener("click", () => {
-    switchSlide(index);
-  });
+    listButton.addEventListener("click", () => {
+        switchSlide(index);
+    });
 });
 
 replacementCitys.forEach((replacementCity, index) => {
-  replacementCity.addEventListener("click", () => {
-    switchSlide(index);
-  });
+    replacementCity.addEventListener("click", () => {
+        switchSlide(index);
+    });
 });
 
 replacementApartmentAreas.forEach((replacementApartmentArea, index) => {
-  replacementApartmentArea.addEventListener("click", () => {
-    switchSlide(index);
-  });
+    replacementApartmentArea.addEventListener("click", () => {
+        switchSlide(index);
+    });
 });
 
 replacementRepairTimes.forEach((replacementRepairTime, index) => {
-  replacementRepairTime.addEventListener("click", () => {
-    switchSlide(index);
-  });
+    replacementRepairTime.addEventListener("click", () => {
+        switchSlide(index);
+    });
 });
 
 handLeft.addEventListener("click", () => {
-  const currentIndex = Array.from(buttons).findIndex((button) =>
-    button.classList.contains("second-section__button--active")
-  );
-  const prevIndex = currentIndex === 0 ? buttons.length - 1 : currentIndex - 1;
-  switchSlide(prevIndex);
+    const currentIndex = Array.from(buttons).findIndex((button) => button.classList.contains("second-section__button--active"));
+    const prevIndex = currentIndex === 0 ? buttons.length - 1 : currentIndex - 1;
+    switchSlide(prevIndex);
 });
 
 handRight.addEventListener("click", () => {
-  const currentIndex = Array.from(buttons).findIndex((button) =>
-    button.classList.contains("second-section__button--active")
-  );
-  const nextIndex = currentIndex === buttons.length - 1 ? 0 : currentIndex + 1;
-  switchSlide(nextIndex);
+    const currentIndex = Array.from(buttons).findIndex((button) => button.classList.contains("second-section__button--active"));
+    const nextIndex = currentIndex === buttons.length - 1 ? 0 : currentIndex + 1;
+    switchSlide(nextIndex);
 });
 
 mobileHandLeft.addEventListener("click", () => {
-  const currentIndex = Array.from(buttons).findIndex((button) =>
-    button.classList.contains("second-section__button--active")
-  );
-  const prevIndex = currentIndex === 0 ? buttons.length - 1 : currentIndex - 1;
-  switchSlide(prevIndex);
+    const currentIndex = Array.from(buttons).findIndex((button) => button.classList.contains("second-section__button--active"));
+    const prevIndex = currentIndex === 0 ? buttons.length - 1 : currentIndex - 1;
+    switchSlide(prevIndex);
 });
 
 mobileHandRight.addEventListener("click", () => {
-  const currentIndex = Array.from(buttons).findIndex((button) =>
-    button.classList.contains("second-section__button--active")
-  );
-  const nextIndex = currentIndex === buttons.length - 1 ? 0 : currentIndex + 1;
-  switchSlide(nextIndex);
+    const currentIndex = Array.from(buttons).findIndex((button) => button.classList.contains("second-section__button--active"));
+    const nextIndex = currentIndex === buttons.length - 1 ? 0 : currentIndex + 1;
+    switchSlide(nextIndex);
 });
 
 switchSlide(0);
